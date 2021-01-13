eeac36042b5765fbf7624aaf4504730b9cb5d66c
alifaleh <alifalih783783@gmail.com>
2021-01-13 15:16:52 +0300

    complete getAllForms, and getFormData

--- src/database.ts
+++ src/database.ts
@@ -1,4 +1,45 @@
+import { getConnection, getRepository } from "typeorm";
 import {Admin} from "./entity/Admin";
 import {Form} from "./entity/Form";
+import { FormData } from "./entity/FormData";
 import {Letter} from "./entity/Letter";
 import {LetterData} from "./entity/LetterData";
+
+
+export const getAllForms = async () => {
+    const allForms = await getConnection()
+    .createQueryBuilder()
+    .select(['form.id', 'form.subject'])
+    .from(Form,'form')
+    .getMany();
+    return allForms;
+}
+
+
+export const getFormData = async (formId) => {
+    const formData = await getConnection()
+    .createQueryBuilder()
+    .select(['form_data.DataName'])
+    .from(FormData, 'form_data')
+    .where(`form_data.formId = '${formId}'`)
+    .getMany();
+    return formData;
+}
+
+
+export const addLetter = async (data,date,formId) => {
+    const addedLetter = await getConnection()
+    .createQueryBuilder()
+    .insert()
+    .into(Letter)
+    .values([{Date:date, signature:'', form:formId}])
+    .returning("id")
+    .execute();
+    const addedLetterId = addedLetter.identifiers[0].id;
+    console.log(data)
+    // data.array.forEach(element => {
+    //     console.log(element);
+    // });
+    return addedLetterId;
+}
+
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {createConnection} from "typeorm";
 import express = require('express');
 import cookieParser = require('cookie-parser');
 import md5 = require('md5');
-import {} from './database'
+import { getAllForms, getFormData, addLetter } from './database'
 
 const app = express();
 app.use(cookieParser());
@@ -18,21 +18,51 @@ const responseCodes={
     invalidCredentials:'2001',
     permitionDenaied:'2002',
     unrecognizedEntry :'2003',
+    invalidFormData:'2004'
 }
 
 createConnection().then(async connection => {
 
     app.get('/', ( req, res ) => {
         res.send('welcome to sawadland official letters system');
-    })
+    });
 
-    app.get('/allforms', ( req, res ) => {
-        res.send('welcome to sawadland official letters system');
-    })
+    app.get('/allforms', async ( req, res ) => {
+        const allForms = await getAllForms();
+        res.json(allForms);
+    });
+
+    app.get('/formdata/:id', async ( req, res ) => {
+        const formData = await getFormData(req.params.id);
+        res.json(formData);
+    });
+
+    app.post('/addletter/:id', async ( req, res) =>{
+        let validData = true;
+        // let date = req.query.date
+        // to be removed:
+        let date = new Date(Date.now())
+        const formData = await getFormData(req.params.id);
+        const data = JSON.parse(req.query.data.toString());
+        let newData:object={};
+        formData.forEach(item => {
+            if(data[item.DataName] == null){
+                validData=false;
+            }else{
+                newData[item.DataName]=data[item.DataName];
+            }
+        })
+        if(validData){
+            const addedLetter = await addLetter(newData, date,req.params.id);
+            res.send(responseCodes.success);
+        }else{
+            res.send(responseCodes.invalidFormData);
+        }
+
+    });
 
     app.listen(port, ()=>{
         console.log('listening on port: ',port );
-    })
-    
+    });
 
 }).catch(error => console.log(error));
