9a1b4101053ceb896447263906b0a540d9a5dbac
alifaleh <ubuntu@DESKTOP-IPGK4P8.localdomain>
2021-01-15 14:53:34 +0300

    cleaning my pants

--- src/makePdf.ts
+++ src/makePdf.ts
@@ -6,26 +6,26 @@ import path       = require('path');
 export const makePdf = (letterNumber, date, subject, greeting, paragraph, footer, letterId) => {
 
   const regularFont = fs.readFileSync(path.join(__dirname,'../assets/fonts/Dubai-Regular.txt'));
-  const boldFont = fs.readFileSync(path.join(__dirname,'../assets/fonts/Dubai-Bold.txt'));
-  const logo = fs.readFileSync(path.join(__dirname,'../assets/images/logo.txt'));
-  var html = fs.readFileSync('template.html', 'utf8');
-  html = html.replace("{{logo}}",logo.toString())
-  html = html.replace("{{regularFont}}",regularFont.toString())
-  html = html.replace("{{boldFont}}",boldFont.toString())
-  html = html.replace("{{letterNumber}}",letterNumber.toString())
-  html = html.replace("{{date}}",date.toString())
-  html = html.replace("{{subject}}",subject.toString())
-  html = html.replace("{{greeting}}",greeting.toString())
-  html = html.replace("{{paragraph}}",paragraph.toString())
-  html = html.replace("{{footer}}",footer.toString())
+  const boldFont    = fs.readFileSync(path.join(__dirname,'../assets/fonts/Dubai-Bold.txt'));
+  const logo        = fs.readFileSync(path.join(__dirname,'../assets/images/logo.txt'));
+  const outputPath  = path.join(__dirname,`../letters/letter_${letterId}.pdf`);
+
+  let html = fs.readFileSync('template.html', 'utf8');
+  html = html.replace(/{{logo}}/g,logo.toString())
+  html = html.replace(/{{regularFont}}/g,regularFont.toString())
+  html = html.replace(/{{boldFont}}/g,boldFont.toString())
+  html = html.replace(/{{letterNumber}}/g,letterNumber.toString())
+  html = html.replace(/{{date}}/g,date.toString())
+  html = html.replace(/{{subject}}/g,subject.toString())
+  html = html.replace(/{{greeting}}/g,greeting.toString())
+  html = html.replace(/{{paragraph}}/g,paragraph.toString())
+  html = html.replace(/{{footer}}/g,footer.toString())
 
   type format = "A4";
   const format:format = "A4";
-  var options = { format: format};
-
-  fs.writeFileSync('../letters/lettertemp.html',html)
+  let options = { format: format};
   
-  pdf.create(html, options).toFile(`../letters/letter_${letterId}.pdf`, function(err, res) {
+  pdf.create(html, options).toFile(outputPath, function(err, res) {
     if (err) return console.log(err);
   });
 
